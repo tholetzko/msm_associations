328d855decc933833c2636cb500b1394b6707128
Tobias <tholetzko@users.noreply.github.com>
2017-05-18 16:15:47 -0500

    Finished project

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -9,7 +9,7 @@ class Movie < ApplicationRecord
   validates :year, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1870, :less_than_or_equal_to => 2050}
 
   # - duration: must be integer between 0 and 2764800, but it can be blank (hint: there is an option `:allow_blank => true`)
-  validates :duration, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 2764800}
+  validates :duration, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 2764800}, :allow_blank => true
 
   # - description: no rules
 
